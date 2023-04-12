bd394904ad39c266afbcbfa02de4da6395cd23a5
mamathakamma <mamatha@hutechsolutions.com>
2023-04-12 15:23:11 +0530

    netflix-zuul-api-gateway-server

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/example/springcloud/example/config/server/ZuulLoggingFilter.java
@@ -0,0 +1,41 @@
+package com.example.springcloud.example.config.server;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request=RequestContext.getCurrentContext().getRequest();	
+		logger.info("request -> {} request uri ->{}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
